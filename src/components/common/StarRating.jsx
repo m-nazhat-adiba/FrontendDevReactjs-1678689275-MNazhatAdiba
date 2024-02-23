3846dd8a4c7c85cc911e159d62e191559cdb1003
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2024-02-23 13:54:09 +0700

    added outlined stars

--- src/components/common/StarRating.jsx
+++ src/components/common/StarRating.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 
 const StarRating = ({ totalStars }) => {
+  const filledStars = Math.floor(totalStars);
+  const outlinedStars = Math.floor(5 - totalStars);
+
   return (
     <div>
-      {[...Array(totalStars)].map((_, index) => (
+      {[...Array(filledStars)].map((_, index) => (
         <span key={index} className="text-blue-900 text-lg">
           ★
+        </span>
+      ))}
+
+      {[...Array(outlinedStars)].map((_, index) => (
+        <span key={index} className="text-gray-400 text-lg">
+          ☆
         </span>
       ))}
     </div>
